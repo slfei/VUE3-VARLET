da894229d0b8e18dcbcf9bd840004a4335624786
slfei <lfshengc@isoftstone.com>
2022-03-11 12:33:47 +0800

    add list

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 const Home = () => import("@/views/home/homeIndex");
 const Login = () => import("@/views/login/loginIndex");
+const List = () => import("@/views/list/listIndex");
 
 const routes = [
   { path: "/", component: Home, name: "主页" },
   { path: "/login", component: Login, name: "登录" },
+  { path: "/list", component: List, name: "列表" },
 ];
 
 export const router = createRouter({
---
+++ src/views/list/listIndex.vue
@@ -0,0 +1,43 @@
+<template>
+  <var-list :finished="finished" v-model:loading="loading" @load="load">
+    <var-cell :key="item" v-for="item in list" @click="goDetail">
+      列表项: {{ item }}
+    </var-cell>
+  </var-list>
+</template>
+<script>
+  import { router } from "@/router";
+  import { ref, reactive } from "vue";
+
+  export default {
+    setup() {
+      const loading = ref(false);
+      const finished = ref(false);
+      const list = reactive([]);
+      const goDetail = () => {
+        router.push("/");
+      };
+      const load = () => {
+        setTimeout(() => {
+          for (let i = 0; i < 20; i++) {
+            list.push(list.length + 1);
+          }
+
+          loading.value = false;
+
+          if (list.length >= 60) {
+            finished.value = true;
+          }
+        }, 1000);
+      };
+
+      return {
+        list,
+        loading,
+        finished,
+        load,
+        goDetail,
+      };
+    },
+  };
+</script>
