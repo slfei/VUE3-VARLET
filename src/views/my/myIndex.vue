c1cddffd6b283183f4573325adc7fdab0c09e797
slfei <lfshengc@isoftstone.com>
2022-03-11 17:07:53 +0800

    add my page

---
+++ src/views/my/myIndex.vue
@@ -0,0 +1,27 @@
+<template>
+  <var-skeleton title avatar card :rows="3" :loading="loading">
+    加载的数据
+  </var-skeleton>
+</template>
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const loading = ref(false);
+
+    const fullscreen = () => {
+      loading.value = true;
+
+      setTimeout(() => {
+        loading.value = false;
+      }, 2000);
+    };
+
+    return {
+      loading,
+      fullscreen,
+    };
+  },
+};
+</script>
